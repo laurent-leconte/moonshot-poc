38ba0a365931a4734667e19a75f37495f7c90fca
Laurent Leconte <laurent.leconte@gmail.com>
2017-03-22 22:26:38 +0100

    fix secret key import

--- moonshot/settings.py
+++ moonshot/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
-import os, secret
+import os
+from secret import SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
