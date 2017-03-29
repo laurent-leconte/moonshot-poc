6ac36e2c3d7d6225c7ea912e6ab853563f5281f7
Laurent Leconte <laurent.leconte@gmail.com>
2017-03-29 18:10:08 +0200

    add a product list page

--- pocsite/urls.py
+++ pocsite/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
      url(r'^$', views.index, name='index'),
+     url(r'^products/$', views.ProductListView.as_view(), name='products'),
+     
 ]
--- pocsite/views.py
+++ pocsite/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Product
 
 def index(request):
     published_product_list = Product.objects.filter(is_published=True)
     products = [str(p) for p in published_product_list]
     return render(request, 'index.html', context={'products':products})
+
+class ProductListView(generic.ListView):
+    model = Product
+    def get_queryset(self):
+        return Product.objects.filter(is_published=True)
