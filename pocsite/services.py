ebcf367fff3e9a95966be03d530a1b13b3df050d
Laurent Leconte <laurent.leconte@gmail.com>
2017-04-04 22:26:46 +0200

    generate quote in services.py

---
+++ pocsite/services.py
@@ -0,0 +1,16 @@
+from .models import Quote, Product, Contract
+
+def generate_quote(product_id):
+    try:
+        #IRL this would call an API or third-party service
+        product = Product.objects.get(id=product_id)
+        if product.is_published:
+            quote = Quote.objects.create(product=product, quote_price=product.base_price,
+             quote_payout=product.base_payout,status='p')
+            return quote
+        else:
+            #TODO : handle non-published products properly
+            return None
+    except Product.DoesNotExist:
+        #TODO : handle error properly
+        return None
--- pocsite/views.py
+++ pocsite/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Product
+from .services import generate_quote
+from django.core.exceptions import ValidationError
 
 def index(request):
     published_product_list = Product.objects.filter(is_published=True)
     products = [str(p) for p in published_product_list]
     return render(request, 'index.html', context={'products':products})
     
-def get_quote(request):
-    pass
+def get_quote(request, pk):
+    quote = generate_quote(pk)
+    print(quote)
+    
 
 class ProductListView(generic.ListView):
     model = Product
