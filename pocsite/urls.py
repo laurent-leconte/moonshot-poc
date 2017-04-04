76fd6ce74f2d0e41671abf9520dfb0268f3c00f1
Laurent Leconte <laurent.leconte@gmail.com>
2017-04-04 17:38:19 +0200

    add url and (empty) view to generate a quote

--- pocsite/urls.py
+++ pocsite/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
      url(r'^$', views.index, name='index'),
      url(r'^products/$', views.ProductListView.as_view(), name='products'),
+     url(r'^products/(?P<pk>[-\w]+)/quote/$', views.get_quote, name='get_quote'),
      
 ]
--- pocsite/views.py
+++ pocsite/views.py
@@ -6,6 +6,9 @@ def index(request):
     published_product_list = Product.objects.filter(is_published=True)
     products = [str(p) for p in published_product_list]
     return render(request, 'index.html', context={'products':products})
+    
+def get_quote(request):
+    pass
 
 class ProductListView(generic.ListView):
     model = Product
